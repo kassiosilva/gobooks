fa9c302dd6345da65ff0c4dc2ed335240a485c10
Kássio Silva <kassio.slva@gmail.com>
2022-09-23 12:37:15 -0300

    Estrutura inicial

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+
+import theme from 'src/theme';
+
+import { Home } from '@screens/Home';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
